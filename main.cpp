7c4c5f1255581e25176e46221f5e44aa01239861
Heinz <ha22457098@gmail.com>
2016-03-22 01:36:29 +0800

    finish main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@
 using namespace std;
 
 int main(){
+	float height,weight;
+	Bmi BmiObject;
+	ifstream input("file.in",ios::in);
+	ofstream output("file.out",ios::trunc);
+	output.close;
 
+	while( (input>>height) && (input>>weight) && (weight!=0) ){
+		BmiObject.setHeightWeight(height,weight);
+		BmiObject.calculate();
+		BmiObject.outputBmi();
+	}
+	input.close();
 	return 0;
 }
