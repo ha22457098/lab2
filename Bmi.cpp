4d823264482eaa1ca2263c9cd6e129d6e1400827
Heinz <ha22457098@gmail.com>
2016-03-22 01:29:13 +0800

    finish Bmi.cpp

--- Bmi.cpp
+++ Bmi.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<fstream>
 #include<math.h>
+#include<iomanip>
 #include"Bmi.h"
 
 using namespace std;
@@ -20,4 +21,42 @@ void Bmi::outputBmi(){
 	output<<fixed<<setprecision(2)<<bmi<<"\t";
 //http://www.programmer-club.com.tw/ShowSameTitleN/cb/17025.html
 	if(bmi<18.5){
+		if(bmi<16){
+			if(bmi<15)
+				output<<"Very severely underweight"<<endl;
+			else
+				output<<"Severely underweight"<<endl;
+		}
+		else{
+			output<<"Underweight"<<endl;
+		}
+	}
+	else{
+		if(bmi>25){
+			if(bmi>30){
+				if(bmi>35){
+					if(bmi>=40)
+						output<<"Obese Class III (Very severely obese)"<<endl;
+					else
+						output<<"Obese Class II (Severely obese)"<<endl;
+				}
+				else{
+					output<<"Obese Class I (Moderately obese)"<<endl;
+				}
+			}
+			else{
+				output<<"Overweight"<<endl;
+			}
+		}
+		else{
+			output<<"Normal"<<endl;
+		}
+	}
+	output.close();
+}
+
+
+
+
+
 
